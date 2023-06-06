57c6d04321f259df0234b991d136a30d5ffd3e5d
justosky <justosky2011@yahoo.com>
2023-06-06 14:27:40 +0000

    Converts a lowercase string to uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for each_character in str:
+        if (ord(each_character) >= 97 and ord(each_character) <= 122):
+            each_character = each_character - 32
+       print("{:s}".format(each_character), end="")
+print("\n", end="")
