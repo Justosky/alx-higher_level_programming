cb7e990d6cbc83964e6732927d2e589e543595d0
justosky <justosky2011@yahoo.com>
2023-06-06 16:05:07 +0000

    teb alpha

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-for ch in range(ord('z'), ord('A')-1, -1):
-    print(f"{chr(ch)}", end="")
+for i in range(26):
+    if i % 2 == 0:
+        print('{:c}'.format(122 - i), end='')
+    else:
+        print('{:c}'.format(90 - i), end='')
