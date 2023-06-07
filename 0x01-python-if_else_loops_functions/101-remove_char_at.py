f50e022a32a8685806e0dcacea95908db87979de
justosky <justosky2011@yahoo.com>
2023-06-07 14:49:28 +0000

    A python script that removes a character at a given index

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    if (n < 0 or n >= len(str)):
-        return (str)
-    new_str = list(str)
-    new_str = new_string.pop(n)
-    new_string = " ".join(new_str)
-    return (new_string)
+    new = ''
+    i = 0
+    for c in str:
+        if i != n:
+            new += str[i]
+        i += 1
+    return new
