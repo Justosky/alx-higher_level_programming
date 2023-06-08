8ea264f8f92b82c1e372ecdd947d20d77693c598
justosky <justosky2011@yahoo.com>
2023-06-08 15:50:05 +0100

    A python script that prints non hidden strings from a module

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if (__name__) == ("__main__"):
     import hidden_
-        for name in dir(hidden_4):
-            if((name[:2]) != "__"):
-                print("{}".format(name))
+     for name in dir(hidden_4):
+         if((name[:2]) != ("__")):
+             print("{}".format(name))
