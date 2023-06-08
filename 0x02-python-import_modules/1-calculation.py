d29f1e7396c4f2f30c1b8ec443092854b685d00b
justosky <justosky2011@yahoo.com>
2023-06-08 11:41:31 +0000

    A python script that imports a module and perform calculations with functions inside the module

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -2,7 +2,7 @@
 import calculator_1
 a = 10
 b = 15
-if (__name__) == ('__main__')
+if (__name__) == ('__main__'):
     print("{} + {} = {}".format(a, (b - a), calculator_1.add(a, (b - a))))
     print("{} - {} = {}".format(a, (b - a), calculator_1.sub(a, (b - a))))
     print("{} * {} = {}".format(a, (b - a), calculator_1.mul(a, (b - a))))
