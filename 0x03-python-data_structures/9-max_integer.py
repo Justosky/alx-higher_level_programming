c4d1be78bbe93d6206853aa2e9e67c14c7b57090
justosky <justosky2011@yahoo.com>
2023-06-09 15:08:00 +0100

    A program that returns the maximum number in a list

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list = []):
     if not my_list:
-        return
+        return (None)
     largest = my_list[0]
     for count in range(1, len(my_list)):
         if my_list[count] > largest:
