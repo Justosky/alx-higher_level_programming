6ccab2bd941862f9069c0bac9b6bebb644165e61
justosky <justosky2011@yahoo.com>
2023-06-11 09:03:04 +0100

    Fixed the last two lines

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 
-def divisible_by_2(my_list = []):
+def divisible_by_2(my_list=[]):
     if my_list == []:
         return
     new_list = my_list.copy()
     for count in range(0,  len(my_list)):
-        if (my_list[count] % 2 ==  0):
+        if (my_list[count] % 2 == 0):
             new_list[count] = 1
         else:
             new_list[count] = 0
-    return  (new_list)
+    return (new_list)
