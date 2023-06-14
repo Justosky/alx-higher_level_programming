683215f24c25da8db9f180445b5357d4c34294c8
justosky <justosky2011@yahoo.com>
2023-06-14 15:23:12 +0100

    My first lambda function

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-    return (list(map((lambda number: number * numberi), num_in_imatrix))
-            for num_in_matrix in matrix)
+    return [list(map((lambda number: number * number), num_in_matrix))
+            for num_in_matrix in matrix]
