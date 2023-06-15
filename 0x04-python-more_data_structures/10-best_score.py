2a7b6f383fcec952719b268405b6f958f558b608
justosky <justosky2011@yahoo.com>
2023-06-15 12:47:55 +0100

    A func that returns the the key with the highest interger value in a dictionary

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -10,10 +10,9 @@ an interger value.
 
 def best_score(a_dictionary):
     if a_dictionary and len(a_dictionary):
-        return (None)
-    else:
         max_key = list(a_dictionary.keys())[0]
         for any_key in a_dictionary:
             if a_dictionary[any_key] > dictionary[max_key]
             max_key = any_key
         return (max_key)
+    return (None)
