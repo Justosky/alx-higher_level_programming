4adbf8a829a9f9734022831884b6c54ce60ea0be
justosky <justosky2011@yahoo.com>
2023-06-15 11:33:43 +0100

    A function that deletes a key and it's value out of a dictionary

---
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+'''
+This function (simple_delete) takes two
+arguments a_dictionary and key
+@a_dictionary: A dictionary.
+@key: The key of the dictionary that we want to delete
+The function checks if the key exis in the
+dictionary and if  the statement evaluates to true
+the function deletes the key along with it's value
+out of the dictionary.
+The function returns the uppdated dictionary.
+'''
+
+def simple_delete(a_dictionary, key=""):
+    if key in a_dictionary:
+        del a_dictionary[key]
+    return (a_dictionary)
