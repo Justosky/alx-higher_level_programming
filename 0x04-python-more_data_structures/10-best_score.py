fdfb21064ff27c96b20c6050928d802a4dd958bd
justosky <justosky2011@yahoo.com>
2023-06-15 13:16:25 +0100

    Uses lambda function to and map functon

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,18 +1,9 @@
 #!/usr/bin/python3
-
-'''
-A function that returns the key in a dictionary with the maximum  interger
-returns none if the dictionary is empty or does not exist.
-this function assumes that all  values for keys in the dictionary contains
-an interger value.
-'''
-
-
-def best_score(a_dictionary):
-    if a_dictionary and len(a_dictionary):
-        max_key = list(a_dictionary.keys())[0]
-        for any_key in a_dictionary:
-            if a_dictionary[any_key] > dictionary[max_key]
-            max_key = any_key
-        return (max_key)
-    return (None)
+def best_score(my_dict):
+    if my_dict and len(my_dict):
+        max = list(my_dict.keys())[0]
+        for key in my_dict:
+            if my_dict[key] > my_dict[max]:
+                max = key
+        return max
+    return None
