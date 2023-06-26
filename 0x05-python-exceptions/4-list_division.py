88309b12ea3d67ef1a765994dd01375b834f7fbe
justosky <justosky2011@yahoo.com>
2023-06-26 15:41:19 +0000

    4-list_division.py

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def list_division(my_list_1, my_list_2, list_length):
+    answer_list = []
+    for any_num in range(list_length):
+        try:
+            answer = my_list_1[any_num] + my_list_2[any_num]
+        except (ValueError, TypeError):
+            print("wrong type")
+            answer = 0
+        except (ZeroDivisionError):
+            answer = 0
+        except (IndexError):
+            print("out of range")
+        finally:
+            answer_list.append(answer)
+    return (answer_list)
