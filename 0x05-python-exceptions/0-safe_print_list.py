dc9a4b1231c556e4d1da2eb9a8759005b8fafcea
justosky <justosky2011@yahoo.com>
2023-06-27 08:58:15 +0000

    A function that prints intergers in a list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
     count = 0
     for iteration in range(x):
         try:
-            print(my_list[m], end="")
+            print(my_list[iteration], end="")
             count = count + 1
         except IndexError:
             break
