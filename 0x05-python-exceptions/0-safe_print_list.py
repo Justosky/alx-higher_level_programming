b74564f26b5f19454c8f295bff912cbc0d52407f
justosky <justosky2011@yahoo.com>
2023-06-27 08:57:03 +0000

    A function that prints intergers in a list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    count = 0 #This variable will be used to count the number of elements printed
+    count = 0
     for iteration in range(x):
         try:
             print(my_list[m], end="")
