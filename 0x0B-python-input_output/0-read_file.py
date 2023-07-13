f842f10dc966fe1728aedd1a6e42614c7338cd3d
justosky <justosky2011@yahoo.com>
2023-07-13 09:13:07 +0000

    A python program that reads file content using UTF-8 encoding

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 
-"""The definition of a text file raeding function """
+"""The definition of a text file raeding function"""
 
 
 def read_file(filename=""):
+    """Function prototype"""
     with open(filename, 'r', encoding='UTF-8') as file:
+        """Creates a file object"""
         print(file.read(), end="")
+        """Prints the contents of the file"""
