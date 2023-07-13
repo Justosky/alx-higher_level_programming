1b8283407161cecf33f767226881c0d34fe90044
justosky <justosky2011@yahoo.com>
2023-07-13 10:11:55 +0000

    A python program that serializes a string into JSON string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import json
+"""Retrieved the JSON library"""
+
+"""The definition of a function that serializes a string into"""
+"""JSON string"""
+
+
+def to_json_string(my_obj):
+    """The function skeleton"""
+    return (json.dumps(my_obj))
