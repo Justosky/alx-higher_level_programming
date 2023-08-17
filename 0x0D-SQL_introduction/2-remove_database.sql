698584140155190efa6cb5b76c65b210fe99bf36
justosky <justosky2011@yahoo.com>
2023-08-17 10:06:33 +0100

    A script that deletes a database from Mysql

--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,4 +1,4 @@
-i-- A script that removes a User from 
+-- A script that removes a User from 
 -- MySQL Database and do not fail if the database do
 -- Does not exist
 DROP DATABASE IF EXISTS 'hbtn_0c_0';
