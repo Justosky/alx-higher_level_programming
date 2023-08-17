16b75f2544f4a573bb9bf204f1cf93bbf8f77331
justosky <justosky2011@yahoo.com>
2023-08-17 12:23:47 +0100

    Orders data from a table in ASC orders

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,6 +1,6 @@
 -- A script that orders data from a table --
 -- In Ascending order --
-SELECT * 
+SELECT `score`, `name`
 FROM `second_table`
 WHERE `score` >= 10
-ORDER BY `score` DESC; 
+ORDER BY `score` DESC;
