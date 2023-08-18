e2b5ab175e4f58a445fb0fe8a74c24a5d7b52741
justosky <justosky2011@yahoo.com>
2023-08-18 12:17:45 +0100

    Cities

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -8,5 +8,5 @@ id INT UNIQUE AUTO_INCREMENT NOT NULL,
 state_id INT NOT NULL,
 name VARCHAR(256) NOT NULL,
 PRIMARY KEY(id),
-FOREIGN KEY(state_id) REFERENCES states(id)
+FOREIGN KEY(state_id) REFERENCES htbn_0d_usa.states(id)
 );
