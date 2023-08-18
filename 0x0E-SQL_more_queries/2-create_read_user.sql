0469f81cd9102ee431b0ee5525e00a1a6ef0a4d6
justosky <justosky2011@yahoo.com>
2023-08-18 11:05:40 +0100

    Creates a user and grants them privileges

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,7 @@
+-- A script that creates a user and database if they didn't exist
+-- And grants the newly created user the SELECT privilege on the
+-- newly created database
+
+CREATE USER IF NOT EXISTS `user_0d_2@localhost`
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_2`
+GRANT SELECT ON `hbtn_0d_2` TO `user_0d_2@localhost`
