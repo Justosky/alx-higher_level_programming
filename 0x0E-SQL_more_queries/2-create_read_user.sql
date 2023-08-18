6e1e9e7e9b6c7bbef7b7a3e7885787226bbbe6c4
justosky <justosky2011@yahoo.com>
2023-08-18 11:08:00 +0100

    Creates a user and grants them privileges

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -2,6 +2,6 @@
 -- And grants the newly created user the SELECT privilege on the
 -- newly created database
 
-CREATE USER IF NOT EXISTS user_0d_2@localhost
-CREATE DATABASE IF NOT EXISTS hbtn_0d_2
-GRANT SELECT ON `hbtn_0d_2` TO `user_0d_2@localhost`
+CREATE USER IF NOT EXISTS user_0d_2@localhost;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+GRANT SELECT ON `hbtn_0d_2` TO `user_0d_2@localhost`;
